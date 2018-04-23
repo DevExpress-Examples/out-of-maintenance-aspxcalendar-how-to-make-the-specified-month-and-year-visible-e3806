a794b84a55433fb132463035f64d891db479437e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WebSite/JScript.js
@@ -0,0 +1,9 @@
+﻿function btn_OnClick(s, e) {
+    if (!ASPxClientEdit.ValidateEditorsInContainer(null))
+        return;
+    var year = cmbYear.GetValue();
+    var month = cmbMonth.GetValue();
+    var day = 10;
+    var myDate = new Date(year, month, day);
+    calendar.SetVisibleDate(myDate);
+}
