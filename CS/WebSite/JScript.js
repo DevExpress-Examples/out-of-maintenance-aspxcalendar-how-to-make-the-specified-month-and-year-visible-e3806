16c8bb0d56c769d315b1cfb2b76aa346a09eb93a
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-19 13:05:51 +0400

    Change example status to out of maintenance

--- CS/WebSite/JScript.js
+++
@@ -1,9 +0,0 @@
-﻿function btn_OnClick(s, e) {
-    if (!ASPxClientEdit.ValidateEditorsInContainer(null))
-        return;
-    var year = cmbYear.GetValue();
-    var month = cmbMonth.GetValue();
-    var day = 10;
-    var myDate = new Date(year, month, day);
-    calendar.SetVisibleDate(myDate);
-}
